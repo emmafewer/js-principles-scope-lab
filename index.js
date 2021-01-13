01d884dad147a75b327d2791b4735a82a8091901
emmafewer <emmafewer20@gmail.com>
2021-01-12 16:50:28 -0800

    commit tree 2e18ab1a49dd4fc5a4150a954e47acfa07a84481

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "Emma";
+const leastFavoriteCustomer = "Alex";
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "Sophie";
+}
+
+function changeLeaseFavoriteCustomer() {
+    leastFavoriteCustomer = "Lando";
+}
